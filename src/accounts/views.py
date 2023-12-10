0ce26cedeae085958cc0ae6c319b100b81af3a19
ioan beilic <ioan.beilic@gmail.com>
2023-12-10 20:53:23 +0100

    signin signout layout

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
 
 
 def signup(request):
-    return render(request, "signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.id_valid():
+            user = form.save()
+
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+
+    return render(request, "signup.html", {'form': form})
 
 
 def signin(request):
-    return render(request, "signup.html")
+    return render(request, "signin.html")
